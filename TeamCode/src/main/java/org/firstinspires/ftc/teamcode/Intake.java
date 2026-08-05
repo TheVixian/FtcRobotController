3be59404e7d2c8666602f3e361a935d4f5a2d296
TheVixian <lucastancila01@gmail.com>
2026-08-05 22:26:41 +0300

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -1,34 +1,43 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.teamcode.Intake.State.REVERSE;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-public class Intake implements RobotModule{
+import org.firstinspires.ftc.robotcore.external.State;
+
+public class Intake {
 
     DcMotor intake;
-
+    private State state;
 
 
     public Intake(HardwareMap hm) {
-        this.intake = hm.get(DcMotor.class,"mch0");
+        this.intake = hm.get(DcMotor.class, "mch0");
+    }
+
+    public enum State {
+
+        IDLE,
+        INTAKE,
+        REVERSE
     }
 
 
 
 
-    public void update(Gamepad gm1){
+    public void update(Gamepad gm1) {
+        switch (state) {
+            case IDLE:
+                intake.setPower(0);
+            case INTAKE :
+                intake.setPower(1);
 
-        if(gm1.left_bumper){
-            intake.setPower(1);
+            case REVERSE :
+                intake.setPower(-1);
         }
-        if(gm1.right_bumper){
-            intake.setPower(-1);
-        }
-        if(gm1.triangle){
-            intake.setPower(0);
-        }
-
     }
 }
